6b590c326c5cf1dad1ed031c1aa7b0946da676af
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2024-02-16 21:56:17 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Button, Text } from "react-native-paper";
+
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  const onIncrementarClick = () => {
+    setCounter(counter + 1);
+  }
+  const onResetarClick = () => {
+    setCounter(0);
+  }
+  const onDecrementarClick = () => {
+    setCounter(counter - 1);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text style={styles.counter_display}>{counter}</Text>
+    //   <Button
+    //     style={styles.button_incrementar}
+    //     onPress={onIncrementarClick}
+    //     title="Incrementar"
+    //   ></Button>
+
+    //   <Button
+    //     style={styles.button_incrementar}
+    //     onPress={onResetarClick}
+    //     title="Resetar"
+    //   ></Button>
+      
+    //   <Button
+    //     style={styles.button_incrementar}
+    //     onPress={onDecrementarClick}
+    //     title="Decrementar"
+    //   ></Button>
+    // </View>
+
+    <SafeAreaView style={styles.container}>
+      <Text variant="displayLarge" style={styles.counter_display}>
+        {counter}
+      </Text>
+      <Text variant="bodyLarge" style={styles.body_display}>
+        Pressione o botão abaixo para incrementar o contador.
+      </Text>
+      <Button onPress={onIncrementarClick} icon="plus" mode="contained">
+        Incrementar
+      </Button>
+    </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    fontFamily: 'Cochin',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button_incrementar: {
+    backgroundColor: '#329da8',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
